30858d87e42c54ec93f5ec4e68aa7a79a288c478
Benjamin Williams <DeanPBR@gmail.com>
2020-09-22 12:55:09 -0400

    Adding shelter test class with a todo list.

---
+++ src/test/java/project/VirtualPetShelterTest.java
@@ -0,0 +1,12 @@
+package project;
+
+public class VirtualPetShelterTest {
+
+    /*
+    To Do List:
+    - Able to add pets
+    - Able to remove a specific pet from shelter given it's name
+    - Able to feed all pets in the shelter.
+    - Return list of pets in shelter.
+     */
+}
